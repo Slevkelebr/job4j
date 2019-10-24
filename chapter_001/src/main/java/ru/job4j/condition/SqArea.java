386cc77b6ee9af7c851f742db9dca6ad63d2982b
Slevkelebr <slevkelebr@yandex.ru>
2019-10-25 01:21:01 +0500

    4.5. Добавил тесты на класс SqArea

--- chapter_001/src/main/java/ru/job4j/condition/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -1,10 +1,11 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(int p, int k) {
-        int height = (p / 2) / (k + 1);
-        int length = height * k;
-        return height * length;
+    public static int square(int p, int k) {
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        int s = w * h;
+        return s;
     }
 
     public static void main(String[] args) {
