fdae997423a15ac3223cd8c74147485f2b372dc7
Slevkelebr <slevkelebr@yandex.ru>
2019-10-30 21:10:01 +0500

    6.4.1. Слово заканчивается с ... [#156317]

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = false;
+        int k = 0;
+        for (int i = 0; i < post.length; i++ ) {
+            if (post[post.length - i - 1] == word[word.length - i - 1]) {
+                k++;
+            }
+            if (k == post.length){
+                result = true;
+            }
+        }
+        return result;
+    }
+}
