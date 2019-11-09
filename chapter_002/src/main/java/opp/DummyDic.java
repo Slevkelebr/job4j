35601de04877edee10ecfaa5ae2987129fb009fa
Slevkelebr <slevkelebr@yandex.ru>
2019-11-09 16:03:31 +0500

    1.4. Вызов метод с возвращаемым типом.[#196934]

---
+++ chapter_002/src/main/java/opp/DummyDic.java
@@ -0,0 +1,15 @@
+package opp;
+
+public class DummyDic {
+
+    public String engToRus(String eng) {
+        return "Неизвестное слово " + eng;
+    }
+
+    public static void main(String[] args) {
+        DummyDic bot = new DummyDic();
+        String question = "WTF?";
+        String word = bot.engToRus(question);
+        System.out.println(word);
+    }
+}
