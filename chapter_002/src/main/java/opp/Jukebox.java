d612378db5837e2156e1f11aa3aa228c293ea314
Slevkelebr <slevkelebr@yandex.ru>
2019-11-09 13:58:05 +0500

    1.3. Вызов метода с аргументами.[#196933]

---
+++ chapter_002/src/main/java/opp/Jukebox.java
@@ -0,0 +1,26 @@
+package opp;
+
+public class Jukebox {
+
+    public void music(int position) {
+        if (position == 1 | position == 2) {
+            if (position == 1) {
+                System.out.println("Пусть бугут неуклюже.");
+            } else {
+                System.out.println("Спокойной ночи.");
+            }
+        } else {
+            System.out.println("Песня не найдена.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Jukebox song = new Jukebox();
+        int[] arraySong = {1, 2, 3};
+        for (int number: arraySong
+             ) {
+            song.music(number);
+        }
+
+    }
+}
