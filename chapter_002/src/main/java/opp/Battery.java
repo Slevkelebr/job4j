8ac8751c0d9057b4f7249c1115296fd12ca70ad1
Slevkelebr <slevkelebr@yandex.ru>
2019-11-10 18:59:23 +0500

    1.7. Состояние объекта.[#196937]

---
+++ chapter_002/src/main/java/opp/Battery.java
@@ -0,0 +1,37 @@
+package opp;
+
+/**
+ * Класс создан, что бы показать как с помощью одного класса можно прописать взаимодействие двух объектов.
+ * @author Slevkelebr.
+ * @since 10.11.2019
+ */
+
+public class Battery {
+    private int load;
+
+    /**
+     * Конструктор класса.
+     * @param size заряд батареи.
+     */
+    public Battery(int size) {
+        this.load = size;
+    }
+
+    /**
+     * Метод производит обмен заряда между двумя батареями.
+     * @param another второй экземпляр батареи.
+     */
+    public void exchange(Battery another) {
+        this.load = this.load - another.load;
+        another.load = another.load + another.load;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(14);
+        Battery second = new Battery(4);
+        System.out.println("first: " + first.load + ". second: " + second.load);
+        first.exchange(second);
+        System.out.println("first: " + first.load + ". second: " + second.load);
+
+    }
+}
---
+++ chapter_002/src/main/java/opp/Jar.java
@@ -0,0 +1,28 @@
+package opp;
+
+/**
+ * Класс создаёт два сосуда, и происходит взаимодействие двух объектво.
+ * @author Slevkelebr.
+ * @since 10.11.2019
+ */
+
+public class Jar {
+    private int value;
+
+    public Jar(int size) {
+        this.value = size;
+    }
+
+    public void pour(Jar another) {
+       this.value = (this.value + another.value) / 2;
+       another.value = this.value;
+    }
+
+    public static void main(String[] args) {
+        Jar first = new Jar(10);
+        Jar second = new Jar(4);
+        System.out.println("first : " + first.value + ". second : " + second.value);
+        first.pour(second);
+        System.out.println("first : " + first.value + ". second : " + second.value);
+    }
+}
