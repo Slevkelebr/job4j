6c04cacd994cdedea992c398f71f6c6609885b84
Slevkelebr <slevkelebr@yandex.ru>
2019-11-13 19:27:21 +0500

    2. Перегрузка конструктора.[#199066]

--- chapter_002/src/main/java/ru/job4j/tracker/Predator.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Predator.java
@@ -3,7 +3,7 @@ package ru.job4j.tracker;
 public class Predator extends Animal {
 
     public Predator() {
-        super();
+        super("name");
         System.out.println("Predator");
     }
 }
