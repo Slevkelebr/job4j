56a3cd3013dd158bc4f6323b2606a11012c2214d
Slevkelebr <slevkelebr@yandex.ru>
2019-11-24 15:00:07 +0500

    Шаблон проектирования - Стратегия(Исправления).[#199488]

--- chapter_002/src/main/java/ru/job4j/stargery/Triangle.java
+++ chapter_002/src/main/java/ru/job4j/stargery/Triangle.java
@@ -5,10 +5,10 @@ public class Triangle implements Shape {
     @Override
     public String draw() {
         StringBuilder pic = new StringBuilder();
-        pic.append("   +   \n");
-        pic.append("  + +  \n");
-        pic.append(" +   + \n");
-        pic.append("+++++++\n");
+        pic.append("   +   " + System.lineSeparator());
+        pic.append("  + +  " + System.lineSeparator());
+        pic.append(" +   + " + System.lineSeparator());
+        pic.append("+++++++" + System.lineSeparator());
         return pic.toString();
     }
 }
