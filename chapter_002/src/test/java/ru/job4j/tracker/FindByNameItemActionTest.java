3e86e4d273c84e0ed4c43fe6c23405570b3a94b5
Slevkelebr <slevkelebr@yandex.ru>
2019-11-26 15:00:57 +0500

    Доработать тесты в StartUITest проверяющие вывод данных в консоль.[#80530]

---
+++ chapter_002/src/test/java/ru/job4j/tracker/FindByNameItemActionTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.tracker;
+
+        import org.junit.Test;
+
+        import java.io.ByteArrayOutputStream;
+        import java.io.PrintStream;
+        import java.util.StringJoiner;
+
+        import static org.hamcrest.core.Is.is;
+        import static org.junit.Assert.*;
+
+public class FindByNameItemActionTest {
+
+    @Test
+    public void whenNameTask() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        Tracker tracker = new Tracker();
+        Item item = new Item("Task1");
+        tracker.add(item);
+        FindByNameItemAction act = new FindByNameItemAction();
+        act.execute(new StubInput(new String[] {"Task1"}), tracker);
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add(item.getName() + " " + item.getId())
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
+    }
+}
