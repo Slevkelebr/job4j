4dd31225b42acd0bbd611fb61cec31fcbd631a15
Slevkelebr <slevkelebr@yandex.ru>
2019-12-01 10:37:40 +0500

    5. Аннотация @Override[#204719]

--- chapter_002/src/main/java/ru/job4j/inheritance/HtmlReport.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/HtmlReport.java
@@ -7,6 +7,7 @@ public class HtmlReport extends TextReport {
         System.out.println(text);
     }
 
+    @Override
     public String generate(String name, String body) {
         return "<h1>" + name + "</h1>" + "<br>" + "<span>" + body + "</span>";
     }
