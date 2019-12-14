4dd17b79f45f9dccea715fdfb61a56ce27371062
Slevkelebr <slevkelebr@yandex.ru>
2019-12-14 14:50:54 +0500

    1. Конвертация листа массивов в один лист Integer[#80523]

--- chapter_003/src/main/java/ru/job4j/collection/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/collection/ConvertList2Array.java
@@ -1,8 +1,20 @@
 package ru.job4j.collection;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConvertList2Array {
+
+    public List<Integer> convert(List<int[]> list) {
+        List<Integer> result = new ArrayList<>();
+        for (int[] i : list) {
+            for (int tmp : i) {
+                result.add(tmp);
+            }
+        }
+        return result;
+    }
+
     public static int[][] toArray(List<Integer> list, int cells) {
         int groups = (int) Math.ceil((double) list.size() / cells);
         System.out.println(groups);
--- chapter_003/src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
@@ -3,9 +3,12 @@ package ru.job4j.collection;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertThat;
 
 @Ignore
@@ -23,5 +26,22 @@ public class ConvertList2ArrayTest {
                 {7, 0, 0}
         };
         assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when2ArraysTo1List() {
+        ConvertList2Array list = new ConvertList2Array();
+        List<int[]> list2 = new ArrayList<>();
+        list2.add(new int[]{1, 2});
+        list2.add(new int[]{3, 4, 5, 6});
+        List<Integer> result = list.convert(list2);
+        List<Integer> expected = new ArrayList<>();
+        expected.add(1);
+        expected.add(2);
+        expected.add(3);
+        expected.add(4);
+        expected.add(5);
+        expected.add(6);
+        assertArrayEquals(expected.toArray(new Integer[0]), result.toArray(new Integer[0]));
     }
 }
