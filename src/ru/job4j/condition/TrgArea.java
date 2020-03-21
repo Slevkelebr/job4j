1cf9caac5300ddaff297614d60426452ffeeaa6e
Slevkelebr <slevkelebr@yandex.ru>
2020-03-21 10:14:21 +0500

    Площадь треугольника. [#175474]

---
+++ src/ru/job4j/condition/TrgArea.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+/**
+ * class TrgArea - расчет площади треугольника.
+ *
+ * @author Sergey Frolov (slevkelebr@yandex.ru)
+ * @version 0.1
+ * @since 21.03.2020
+ */
+public class TrgArea {
+    public static double area(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        double rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        double rsl = TrgArea.area(2, 2, 2);
+        System.out.println("area (2, 2, 2) = " + rsl);
+    }
+}
