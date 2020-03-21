f5d9d990dc607bc8d9de47f75b69af63fd88f068
Slevkelebr <slevkelebr@yandex.ru>
2020-03-21 11:17:49 +0500

    Конвертер валюты [#41583] Доработка(методы конвертации валюты в рубли JavaDoc)

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -9,12 +9,40 @@ package ru.job4j.converter;
  */
 public class Converter {
 
+    /**
+     * конвертирует рубли в евро.
+     * @param value рубли.
+     * @return евро.
+     */
     public static int rubleToEuro(int value) {
         return value / 90;
     }
 
+    /**
+     * конвертирует рубли в доллары.
+     * @param value рубли.
+     * @return доллары.
+     */
     public static int rubleToDollar(int value) {
         return value / 80;
+    }
+
+    /**
+     * ковертирует евро в рубли.
+     * @param value евро.
+     * @return рубли.
+     */
+    public static int euroToRuble(int value) {
+        return value * 90;
+    }
+
+    /**
+     * конвертирует доллары в рубли.
+     * @param value доллары
+     * @return рубли
+     */
+    public static int dollarToRuble(int value) {
+        return value * 80;
     }
 
     public static void main(String[] args) {
