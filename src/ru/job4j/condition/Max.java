5f140be39feb5dc649d0bcecd4284e9139be286e
Slevkelebr <slevkelebr@yandex.ru>
2020-03-23 18:11:11 +0500

    Перегрузить метод max для трех чисел [#122644]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,15 +1,44 @@
 package ru.job4j.condition;
 
 /**
- * class Max - наибольшее число из двух.
+ * class Max - наибольшее число из двух, трех и четверых чисел.
  *
  * @author Sergey Frolov (slevkelebr@yandex.ru)
- * @version 0.1
- * @since 22.03.2020
+ * @version 0.2
+ * @since 23.03.2020
  */
 public class Max {
-    public static int max(int left, int right) {
-        int result = left > right ? left : right;
-        return result;
+
+    /**
+     * Вычисляет максимум из двух чисел.
+     * @param first первое число.
+     * @param second второе число.
+     * @return максимальное число.
+     */
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+
+    /**
+     * Перегруженный метод max для вычисления максимального числа из трёх.
+     * @param first первое число.
+     * @param second второе число.
+     * @param third третье число.
+     * @return максимальное число.
+     */
+    public int max(int first, int second, int third) {
+        return max(first, max(second, third));
+    }
+
+    /**
+     * Перегруженный метод max для вычисления максимального числа из четырех.
+     * @param first первое число.
+     * @param second второе число.
+     * @param third третье число.
+     * @param four четвертое число.
+     * @return максимальное число.
+     */
+    public int max(int first, int second, int third, int four) {
+        return max(first, max(second, third, four));
     }
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -7,19 +7,29 @@ import static org.junit.Assert.assertThat;
 public class MaxTest {
     @Test
     public void whenMax1To2Then2() {
-        int result = Max.max(1, 2);
+        Max maximum = new Max();
+        int result = maximum.max(1, 2);
         assertThat(result, is(2));
     }
 
     @Test
-    public void whenMax1To2Then1() {
-        int result = Max.max(5, 2);
+    public void whenMax1To3Then3() {
+        Max maximum = new Max();
+        int result = maximum.max(5, 2, 6);
+        assertThat(result, is(6));
+    }
+
+    @Test
+    public void whenMax1To4Then3() {
+        Max maximum = new Max();
+        int result = maximum.max(2, 2, 5, 3);
         assertThat(result, is(5));
     }
 
     @Test
-    public void whenMax1To2NumberEqual() {
-        int result = Max.max(2, 2);
+    public void whenMax1To4NumberEqual() {
+        Max maximum = new Max();
+        int result = maximum.max(2, 2, 2, 2);
         assertThat(result, is(2));
     }
 }
