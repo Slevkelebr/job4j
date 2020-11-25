12d04d2f4bf5a082998bda0cdcca8311e3ec3818
Slevkelebr <Slevkelebr@yandex.ru>
2020-11-25 22:30:38 +0500

    3.1. Добавить изменения в последний коммит.

---
+++ src/ru/job4j/testbranch/HelloWorld.java
@@ -0,0 +1,8 @@
+package ru.job4j.testbranch;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Thank you Job4j!");
+    }
+}
