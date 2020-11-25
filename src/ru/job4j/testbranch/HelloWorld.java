4de1b7cb5e91f81ce8b74fd5a5a70e785da32a35
Slevkelebr <Slevkelebr@yandex.ru>
2020-11-25 22:44:08 +0500

    3.2. Откат изменений.

--- src/ru/job4j/testbranch/HelloWorld.java
+++ src/ru/job4j/testbranch/HelloWorld.java
@@ -4,5 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Thank you Job4j!");
+
+        System.out.println("I feel so happy");
+        System.out.println("because I found a job as a programmer");
     }
 }
